d70189fe41d96eacabc18f85d747c803c6d96de9
ComitialSign <gabrielpalmeiranaza0@gmail.com>
2024-05-01 21:38:14 -0300

    First commit

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+// imports
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Seja bem vindo!" });
+});
+
+app.listen(3000);
