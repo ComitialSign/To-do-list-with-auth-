a37ba3da51cb0b7686c2072f694cc963739d700d
ComitialSign <gabrielpalmeiranaza0@gmail.com>
2024-05-03 23:08:38 -0300

    token criado, validacao de usuarios cadastrados criado, verificicao de token criado e rota para a pagina do usuario criado

--- backend/app.js
+++ backend/app.js
@@ -35,9 +35,9 @@ app.post("/register", async (req, res) => {
   }
 
   //verificar se o usuario existe
-  const userExists = await User.findOne({ username: username });
+  const userExist = await User.findOne({ username: username });
 
-  if (userExists) {
+  if (userExist) {
     return res.status(422).json({ msg: "Username already in use" });
   }
 
@@ -55,12 +55,83 @@ app.post("/register", async (req, res) => {
     await user.save();
     res.status(201).json({ msg: "User created with success" });
   } catch (err) {
-    console.log(err);
     res
       .status(500)
       .json({ msg: "An error occurred on the server, please try again later" });
   }
 });
+
+//logar na conta
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  //validação
+  if (!username) {
+    return res.status(422).json({ msg: "Username is required" });
+  }
+  if (!password) {
+    return res.status(422).json({ msg: "Password is required" });
+  }
+
+  //verifica se o usuario existe
+  const user = await User.findOne({ username: username });
+
+  if (!user) {
+    return res.status(422).json({ msg: "Invalid username" });
+  }
+
+  //verifica se a senha existe
+  const checkPassword = await bcrypt.compare(password, user.password);
+
+  if (!checkPassword) {
+    return res.status(422).json({ msg: "Incorrect password" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      secret
+    );
+
+    res.status(200).json({ msg: "Authentication success", token });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ msg: "An error occurred on the server, please try again later" });
+  }
+});
+
+//vai para a pagina do usuario
+app.get("/:id", checkToken, async (req, res) => {
+  const id = req.params.id;
+
+  //Verifica se usuario existe
+  const user = await User.findById(id, `-password`);
+
+  if (!user) {
+    return res.status(404).json({ msg: "User not found" });
+  }
+});
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: "Denied access" });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    jwt.verify(token, secret);
+    next();
+  } catch (err) {
+    return res.status(400).json({ msg: "Invalid token" });
+  }
+}
 
 //conectar ao banco
 const dbUrl = process.env.DB_URL;
